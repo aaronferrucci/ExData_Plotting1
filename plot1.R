8d8e868b6ddced0c64724db43168f2ad30a74e97
Aaron Ferrucci <aaronf@antfarm.org>
2015-04-11 15:15:46 -0700

    Remove unused ylim; set bg to transparent (doesn't appear to work).

--- plot1.R
+++ plot1.R
@@ -1,9 +1,11 @@
 source("read_data.R")
+par(
+  bg="transparent"
+)
 png("plot1.png")
 hist(
   main="Global Active Power",
   xlab="Global Active Power (kilowatts)",
-  # ylim=c(0, 1300),
   col=2,
   data$Global_active_power
 )
